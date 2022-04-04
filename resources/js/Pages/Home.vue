9b8dd184d40443621379ba04bb40142249d88b53
Yoeri Boven <yoeriboven@msn.com>
2022-04-04 15:58:13 +0200

    Installed Inertia.js

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,7 @@
+<template>
+    <h1 class="text-3xl text-emerald-500">Test</h1>
+</template>
+
+<script setup>
+
+</script>
--- webpack.mix.js
+++ webpack.mix.js
@@ -12,10 +12,12 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
+    .vue()
     .postCss('resources/css/app.css', 'public/css', [
         require("tailwindcss"),
     ])
     .browserSync({
         proxy: 'yoerime.test',
         notify: false,
-    });
+    })
+    .version();
