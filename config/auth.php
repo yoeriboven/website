53199f20ec375a6f08079ecbfb9ee7dfffccaa78
Yoeri Boven <yoeriboven@msn.com>
2022-04-05 15:30:53 +0200

    Installed Statamic

--- config/auth.php
+++ config/auth.php
@@ -61,9 +61,12 @@ return [
 
     'providers' => [
         'users' => [
-            'driver' => 'eloquent',
-            'model' => App\Models\User::class,
+            'driver' => 'statamic',
         ],
+//        'users' => [
+//            'driver' => 'eloquent',
+//            'model' => App\Models\User::class,
+//        ],
 
         // 'users' => [
         //     'driver' => 'database',
