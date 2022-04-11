014fda2104f177b4962a0ffd1fa6b3fca7904080
Yoeri Boven <yoeriboven@msn.com>
2022-04-11 15:31:00 +0200

    Added translation helper

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,7 @@
 import { createApp, h } from 'vue'
 import { createInertiaApp, Link } from "@inertiajs/inertia-vue3";
 import { InertiaProgress } from '@inertiajs/progress'
+import { i18nVue} from "laravel-vue-i18n";
 
 InertiaProgress.init()
 
@@ -9,6 +10,9 @@ createInertiaApp({
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
             .use(plugin)
+            .use(i18nVue, {
+                resolve: lang => import(`../../lang/${lang}.json`),
+            })
             .mixin({ methods: { route } })
             .component('Link', Link)
             .mount(el)
