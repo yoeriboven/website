6ec89648f57c86f496c2effad74d65be9bae1188
Yoeri Boven <yoeriboven@msn.com>
2022-04-19 17:24:30 +0200

    Added prose img styles

--- resources/js/Pages/Article.vue
+++ resources/js/Pages/Article.vue
@@ -2,7 +2,7 @@
 <div class="min-h-full bg-violet-50">
     <Header />
 
-    <section class="py-6 w-2/3 mx-auto prose">
+    <section class="py-6 w-2/3 mx-auto prose prose-img:shadow-sm prose-img:shadow-indigo-100">
         <p class="">{{ formatDate(article.date, "D MMMM YYYY") }}</p>
 
         <h1>{{ article.title }}</h1>
