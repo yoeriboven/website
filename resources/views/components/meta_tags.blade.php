dfe7329bd5667a513bf398b68f387b3ac6e80c00
Yoeri Boven <yoeriboven@msn.com>
2022-05-12 13:49:05 +0200

    fix

--- resources/views/components/meta_tags.blade.php
+++ resources/views/components/meta_tags.blade.php
@@ -7,8 +7,8 @@
     <meta property="og:image" content="{{ asset("img/social/{$article['slug']}.jpeg") }}">
     <meta property="og:url" content="{{ url()->current() }}">
     <meta property="article:author" content="Yoeri Boven">
-    <meta property="article:published_time" content="{{ $article['publish_date'] }}">
-    <meta property="article:modified_time" content="{{ $article['publish_date'] }}">
+    <meta property="article:published_time" content="{{ $article['publish_date']->toAtomString() }}">
+    <meta property="article:modified_time" content="{{ $article['publish_date']->toAtomString() }}">
     <meta name="twitter:card" content="summary_large_image">
 @endif
 
