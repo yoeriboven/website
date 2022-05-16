4edf87ebacc9600029d6d71fb3db099142720c34
Yoeri Boven <yoeriboven@msn.com>
2022-05-16 15:40:27 +0200

    Styled project modal

--- resources/js/Shared/ContactSuccessModal.vue
+++ resources/js/Shared/ContactSuccessModal.vue
@@ -6,7 +6,7 @@
             </TransitionChild>
 
             <div class="fixed z-10 inset-0 overflow-y-auto">
-                <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+                <div class="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
                 <!-- This element is to trick the browser into centering the modal contents. -->
                     <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
                     <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" enter-to="opacity-100 translate-y-0 sm:scale-100" leave="ease-in duration-200" leave-from="opacity-100 translate-y-0 sm:scale-100" leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
--- resources/js/Shared/TopBanner.vue
+++ resources/js/Shared/TopBanner.vue
@@ -3,7 +3,7 @@
         <div class="max-w-7xl mx-auto py-2 px-3 sm:px-6 lg:px-8">
             <div class="text-center sm:px-16">
                 <div class="font-medium text-white flex flex-col md:inline-flex md:flex-row">
-                    <div class="flex items-center space-x-2">
+                    <div class="flex items-center justify-center space-x-2">
                         <img class="cursor-text"
                              style="height:1.25em;"
                              src="/img/waving-hand-120.png"
@@ -12,7 +12,7 @@
                         <span>I'm currently available for new projects!</span>
                     </div>
                     <span class="block sm:ml-2 sm:inline-block">
-                        <Link :href="route('contact')" class="text-white font-bold underline">
+                        <Link :href="route('contact')" class="text-white font-bold underline underline-offset-2">
                             Hire me <span aria-hidden="true">&rarr;</span>
                         </Link>
                     </span>
