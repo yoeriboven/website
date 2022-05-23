b5f207d1614355a71dd3a89e51e4dc0b0c264a9b
Yoeri Boven <yoeriboven@msn.com>
2022-05-23 15:08:25 +0200

    Style fixes

--- resources/js/Shared/ProjectModal.vue
+++ resources/js/Shared/ProjectModal.vue
@@ -12,28 +12,26 @@
                     <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" enter-to="opacity-100 translate-y-0 sm:scale-100" leave="ease-in duration-200" leave-from="opacity-100 translate-y-0 sm:scale-100" leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
                         <DialogPanel class="relative max-h-[35rem] overflow-y-scroll inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6">
                             <div>
-                                <div class="">
-                                    <div>
-                                        <DialogTitle as="h3" class="text-lg leading-6 font-medium text-gray-700">
-                                            {{ featuredProject.title }}
-                                        </DialogTitle>
-                                        <span class="text-gray-500">{{ featuredProject.repo }}</span>
-                                    </div>
+                                <div>
+                                    <DialogTitle as="h3" class="text-xl leading-6 font-semibold text-gray-700">
+                                        {{ featuredProject.title }}
+                                    </DialogTitle>
+                                    <span class="text-gray-500">{{ featuredProject.repo }}</span>
+                                </div>
 
-                                    <div class="mt-2 prose focus:prose-a:outline-none">
-                                        <p v-html="featuredProject.content" class="text-sm text-gray-500"></p>
-                                    </div>
+                                <div class="mt-2 prose focus:prose-a:outline-none">
+                                    <p v-html="featuredProject.content" class="text-sm text-gray-500"></p>
                                 </div>
                             </div>
                             <div class="mt-5 sm:mt-6 space-y-2">
                                 <a :href="featuredProject.link"
                                    target="_blank"
-                                   class="inline-flex justify-center w-full rounded-md shadow-sm px-4 py-2 border border-gray-300 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 sm:text-sm">
+                                   class="inline-flex justify-center w-full rounded-md shadow-sm px-4 py-2 border border-gray-300 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none sm:text-sm">
                                     View code
                                 </a>
                                 <button
                                       @click="$emit('close')"
-                                      class="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm">
+                                      class="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none sm:text-sm">
                                     Go back
                                 </button>
                             </div>
