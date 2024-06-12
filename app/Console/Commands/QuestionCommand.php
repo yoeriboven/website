8fefa7c5ab382e63f438aa19b892652b8076788c
Yoeri Boven <yoeriboven@msn.com>
2024-06-12 18:16:50 +0200

    Create QuestionCommand.php

---
+++ app/Console/Commands/QuestionCommand.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class QuestionCommand extends Command
+{
+    protected $signature = 'question';
+
+    protected $description = 'Command description';
+
+    public function handle(): void
+    {
+        $answer = $this->ask('Do you want to alert users of high heart risk? Only do this when the heart risk calculations have changed. (y/n)');
+
+        if (in_array($answer, ['y', 'Y', '1', 'yes'])) {
+            $this->info('Show notification');
+        } else {
+            $this->info('Do not show notification');
+        }
+
+        dump($answer);
+    }
+}
