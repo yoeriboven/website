d257afffe61d1377251e5ddcaf153ab68d241b76
Yoeri Boven <yoeriboven@msn.com>
2024-06-12 18:42:29 +0200

    Update QuestionCommand.php

--- app/Console/Commands/QuestionCommand.php
+++ app/Console/Commands/QuestionCommand.php
@@ -12,34 +12,43 @@ class QuestionCommand extends Command
 
     public function handle(): void
     {
+        if ($a = $this->shouldShowHighHeartRiskNotification()) {
+            $this->info('Show notification');
+        } else {
+            $this->info('Do not show notification');
+        }
+
+        dump($a);
+    }
+
+    public function shouldShowHighHeartRiskNotification(): bool
+    {
         try {
 
             $this->info('ja');
 
             $answer = $this->ask('Do you want to alert users of high heart risk? Only do this when the heart risk calculations have changed. (y/n)', null);
-
+//
             $this->info('x');
-
-            if ($answer === null) {
-                $this->error('Run from the command line.');
-            }
+//
+//            if ($answer === null) {
+//                $this->error('Run from the command line.');
+//            }
         } catch (\Exception $exception) {
             $answer = null;
         }
 
         $this->info('test');
-        
+
         if ($answer === null) {
             $this->error('Run from the command line.');
         }
 
 
         if (in_array($answer, ['y', 'Y', '1', 'yes'])) {
-            $this->info('Show notification');
+            return true;
         } else {
-            $this->info('Do not show notification');
+            return false;
         }
-
-        dump($answer);
     }
 }
